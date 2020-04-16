a9d1941ecd735a113f8d7bfa390d6f493b5fda01
VorapratR <vorapratrattana@gmai.com>
2020-04-16 14:59:28 +0700

    Initial Commit

---
+++ BMICal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMICal
+//
+//  Created by Woraprat Rattana on 16/4/2563 BE.
+//  Copyright © 2563 Woraprat Rattana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
